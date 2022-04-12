ab4168501131da23c73fa3d7f3db02736aeaaf41
sarah <sarah@mobidoo.co.kr>
2022-04-13 00:07:19 +0900

    feat: reset.css 적용, Layout 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return <div className="App">Hello React!</div>;
+  return (
+    <div className="App">
+      <Layout>
+        <div>hello react!</div>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
