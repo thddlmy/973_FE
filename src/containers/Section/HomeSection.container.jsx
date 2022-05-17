3160a12c56486933828dddedef53154bf52c7360
SONG YI PARK <69751205+thddlmy@users.noreply.github.com>
2022-05-17 21:25:31 +0900

    refactor: 홈 컨테이너 구조 변경
    * feat: 회원가입 이메일, 닉네임 중복확인 api 연동 및 내 정보 수정 닉네임 중복 확인 연동, 내 정보 수정 페이지 레이아웃 변경
    
    * feat: author 수정, 검색 기능 구현
    
    * refactor: 홈페이지 컨테이너 구조 변경

--- src/containers/Section/HomeSection.container.jsx
+++ src/containers/Section/HomeSection.container.jsx
@@ -5,37 +5,31 @@ import { useForm } from '@hooks';
 import { searchPost } from '@apis/search';
 
 const HomeSectionContainer = (props) => {
-  const {
-    values,
-    setValues,
-    errors,
-    handleChange,
-    handleSubmit,
-    handleListClick,
-  } = useForm({
-    initialValues: {
-      keyword: '',
-      sport: [],
-      location: [],
-      type: '',
-    },
-    onClick: (e) => {
-      console.log(e);
-    },
-    onSubmit: ({ keyword, sport, location, type }) => {
-      console.log(keyword, sport, location, type);
-      const response = searchPost({
-        keyword,
-        sport,
-        location,
+  const { values, setValues, handleChange, handleSubmit, handleListClick } =
+    useForm({
+      initialValues: {
+        keyword: '',
+        sport: [],
+        location: [],
         type: '',
-        page: 1,
-      });
-      console.log(response);
-      setValues({});
-    },
-    validate: () => {},
-  });
+      },
+      onClick: (e) => {
+        console.log(e);
+      },
+      onSubmit: ({ keyword, sport, location, type }) => {
+        console.log(keyword, sport, location, type);
+        const response = searchPost({
+          keyword,
+          sport,
+          location,
+          type: '',
+          page: 1,
+        });
+        console.log(response);
+        setValues({});
+      },
+      validate: () => {},
+    });
   return (
     <>
       <Banner />
